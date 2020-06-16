7baaf532b9fd056b6b7bd21b54c73fe314bef5c5
guimcaballero <guim.caballero@gmail.com>
2020-06-16 22:49:56 +0200

    Implemented first version with db

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,24 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( chat
     ) where
 
 import Data.Text(pack, unpack, replace)
 import Database.SQLite.Simple
-import Database.SQLite.Simple.FromRow
 
-data State = State { currentTopic::String, name::String } deriving (Show)
+data State = State { currentTopic::Int, name::String } deriving (Show)
 
-data KnowledgeUnit = KnowledgeUnit { id::Int, input::String, output::String, topic::Int, gotoTopic::Int } deriving (Show)
+data KnowledgeUnit = KnowledgeUnit { unitId::Int, userInput::String, kellyOutput::String, topic::Int, gotoTopic::Int } deriving (Show)
 
 instance FromRow KnowledgeUnit where
-  fromRow = KnowledgeUnit <$> field <*> field
+  fromRow = KnowledgeUnit <$> field <*> field <*> field <*> field <*> field
+instance ToRow KnowledgeUnit where
+  toRow (KnowledgeUnit id' userInput' kellyOutput' topic' gotoTopic') = toRow (id', userInput', kellyOutput', topic', gotoTopic')
+
 
 chat :: IO ()
 chat = do
-    putStrLn "Hello! I'm not Zeve!"
+    putStrLn "Hello! I'm Kelly!"
     _name <- askName
-    let state = State "General" _name
+    let state = State 1 _name
     putStrLn $ processAnswer "Hello &&name!" state
     putStrLn "How are you today?"
     mainLoop state
@@ -30,9 +33,9 @@ mainLoop state = do
         putStrLn "Bye!"
         return ()
     else do
-        let answer = findAnswer input
-        putStrLn $ "Z: " ++ processAnswer answer state
-        mainLoop state
+        answer <- findAnswer input state
+        putStrLn $ "Z: " ++ processAnswer ( kellyOutput answer ) state
+        mainLoop $ changeTopic (unitId answer) state
 
 askName :: IO String
 askName = do
@@ -48,33 +51,39 @@ promptLine prompt = do
     putStr prompt
     getLine
 
-
-getKUs :: IO ()
-getKUs = do
-  conn <- open "test.db"
-  execute conn "INSERT INTO test (str) VALUES (?)"
-    (Only ("test string 2" :: String))
-  r <- query_ conn "SELECT * from test" :: IO [KnowledgeUnit]
-  mapM_ print r
+findAnswer :: String -> State -> IO KnowledgeUnit
+findAnswer text state = do
+  conn <- open "database.sqlite"
+  exactKus <- query_ conn $ ( Query . pack ) ("SELECT * from knowledge_units where userInput='" ++ text ++ "' and topic=" ++ show (currentTopic state)) :: IO [KnowledgeUnit]
+  likeKus <- query_ conn $ ( Query . pack ) ("SELECT * from knowledge_units where userInput like '%" ++ text ++ "%' and topic=" ++ show (currentTopic state)) :: IO [KnowledgeUnit]
+  exactKusInGeneral <- query_ conn $ ( Query . pack ) ("SELECT * from knowledge_units where userInput='" ++ text ++ "' and topic=1") :: IO [KnowledgeUnit]
+  likeKusInGeneral <- query_ conn $ ( Query . pack ) ("SELECT * from knowledge_units where userInput like '%" ++ text ++ "%' and topic=1") :: IO [KnowledgeUnit]
   close conn
+  return $ returnAnswerFromSearch ( exactKus ++ likeKus ++ exactKusInGeneral ++ likeKusInGeneral )
 
-
-
-
-
-
-findAnswer :: String -> String
-findAnswer text = "Hello &&name!"
+returnAnswerFromSearch :: [KnowledgeUnit] -> KnowledgeUnit
+returnAnswerFromSearch (ku:_) = ku
+returnAnswerFromSearch [] = KnowledgeUnit (-1) "" "Sorry &&name, I'm afraid I couldn't understand you." 1 (-1)
 
 processAnswer :: String -> State -> String
 processAnswer text state = replaceName state (replaceCurrentTopic state text)
 
+changeTopic :: Int -> State -> State
+changeTopic (-1) state = state
+changeTopic newTopic state = State newTopic (name state)
+
 replaceName :: State -> String -> String
-replaceName state text = replaceVar state text "&&name" name
+replaceName state text = replaceVar state text "&&name" ( Left . name )
 
 replaceCurrentTopic :: State -> String -> String
-replaceCurrentTopic state text = replaceVar state text "&&currentTopic" currentTopic
+replaceCurrentTopic state text = replaceVar state text "&&currentTopic" ( Right . currentTopic )
 
+replaceVar :: State -> String -> String -> (State -> Either String Int) -> String
+replaceVar state text var f = unpack $ replace (pack var) value (pack text)
+    where value = pack $ showM val
+          val = f state
 
-replaceVar :: State -> String -> String -> (State -> String) -> String
-replaceVar state text var f = unpack $ replace (pack var) (pack $ f state) (pack text)
+showM :: Either String Int -> String
+showM x = case x of
+    Left y -> y
+    Right y -> show y
